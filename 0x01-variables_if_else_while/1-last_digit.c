8882133e920eb33ba10e77e9f95c19152fe983e8
graphixeye1 <graphixeye1@gmail.com>
2022-03-11 15:55:50 -0800

    message one correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,6 @@
 /**
  * main - finds the last int in a random number
  *
- * Description: The numbers are generated automatically
  * Return: Always(0) Success
  */
 
@@ -19,15 +18,16 @@ int main(void)
 	printf("Last digit of %d is %d and is ", n, last_digit);
 	if (last_digit > 5)
 	{
-		printf("grater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
-	else if (last_digit  == 0)
+	else if(last_digit == 0)
 	{
-		printf("0\n");
+		pritf("Last digit of %d is %d  and is 0\n", n, last_digit);
 	}
-	else if (last_digit < 6 && last_digit != 0)
+	else if(last_digit < 6 && last_digit != 0)
 	{
-		printf("less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n last_digit);
 	}
+
 	return (0);
 }
