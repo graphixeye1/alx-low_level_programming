56cd19755932dbc8cef9f828ab1c008dad35db86
graphixeye1 <graphixeye1@gmail.com>
2022-03-16 19:22:17 -0700

    correction on 2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print the alphabet 10 times
- * Description: using _putchar
- * Return: Always 0 (success)
+ * print_alphabet_x10 - print the alphabet 10 time
+ * Descreption: using _putchar
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
 	char c;
 	int i;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
