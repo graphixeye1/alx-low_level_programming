71105f6dfbfdd9d4242cba5c20325555ca4a2b0c
graphixeye1 <graphixeye1@gmail.com>
2022-05-01 19:59:52 -0700

    correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -200,7 +200,7 @@ int check_elf(char *ptr)
 }
 
 /**
- * main - check the code for ALX-SE School students.
+ * main - check the code for Holberton School students.
  * @argc: number of arguments.
  * @argv: arguments vector.
  * Return: Return 0.
